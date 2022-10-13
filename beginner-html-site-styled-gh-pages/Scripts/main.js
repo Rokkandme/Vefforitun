a0a9003b51192184dd7654015525a0045804ba1d
Rokkandme <dagurtumi@gmail.com>
2022-10-13 17:18:58 +0000

    s

--- beginner-html-site-styled-gh-pages/Scripts/main.js
+++ beginner-html-site-styled-gh-pages/Scripts/main.js
@@ -1,2 +1,22 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
+
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result;}
+
+/*document.querySelector("img").addEventListener("click", function () {
+    alert("Ouch! Stop poking me!");
+  });*/
+
+
+  const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/pp.jpg");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
